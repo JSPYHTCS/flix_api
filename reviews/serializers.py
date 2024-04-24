45c8efe94a691dbba47ad52e5c5c3e2d7bae4aeb
Mateus Santos <receiveai@gmail.com>
2024-04-24 01:09:30 -0300

    Movie all

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 from reviews.models import Review
-from movies.serializers import MovieListDetailSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    movie = MovieListDetailSerializer()
 
     class Meta:
         model = Review
