0b81ca3409afc56b9f17395dd8616684fb66ec58
Mateus Santos <receiveai@gmail.com>
2024-04-24 12:55:52 -0300

    ..

--- reviews/models.py
+++ reviews/models.py
@@ -4,7 +4,7 @@ from movies.models import Movie
 
 
 class Review(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, related_name='reviews')
     stars = models.IntegerField(
         validators=[
             MinValueValidator(0, 'Avaliação não pode ser inferior a 0 estrelas.'),
