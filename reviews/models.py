27c29936e364a2e387a3418bd58d05f55354ffbf
Mateus Santos <receiveai@gmail.com>
2024-04-24 02:02:19 -0300

    normal

--- reviews/models.py
+++ reviews/models.py
@@ -4,7 +4,6 @@ from movies.models import Movie
 
 
 class Review(models.Model):
-    title = models.ManyToManyField(Movie, related_name='movies')
     movie = models.ForeignKey(Movie, on_delete=models.PROTECT, related_name='reviews')
     stars = models.IntegerField(
         validators=[
