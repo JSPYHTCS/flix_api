6ca1afec1d2908292888e8f88c42e3f2b63943de
Mateus Santos <receiveai@gmail.com>
2024-04-24 13:41:53 -0300

    .

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,21 +1,9 @@
 from rest_framework import serializers
 from reviews.models import Review
-from movies.models import Movie
-from movies.serializers import MovieListDetailSerializer 
-
 
 class ReviewSerializer(serializers.ModelSerializer):
-    movie_title = serializers.SerializerMethodField()
+    movie_title = serializers.CharField(source='movie.title', read_only=True)  # Campo para o título do filme
 
     class Meta:
         model = Review
         fields = ['id', 'movie', 'stars', 'comment', 'movie_title']
-    
-    def get_movie_title(self, obj):
-        # Obtém o objeto do filme associado à avaliação
-        movie = Movie.objects.get(pk=obj.movie_id)
-        # Serializa o objeto do filme para obter seus dados, incluindo o título
-        serializer = MovieListDetailSerializer(movie)
-        # Retorna o título do filme
-        return serializer.data['title']
-
