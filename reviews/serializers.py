165c7167fb4cf59601775b2653fc7a2b9b678aea
Mateus Santos <receiveai@gmail.com>
2024-04-24 13:45:46 -0300

    .

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from reviews.models import Review
 
+
 class ReviewSerializer(serializers.ModelSerializer):
-    movie_title = serializers.CharField(source='movie.title', read_only=True)  # Campo para o título do filme
 
     class Meta:
         model = Review
-        fields = ['id', 'movie', 'stars', 'comment', 'movie_title']
+        fields = '__all__'
+
