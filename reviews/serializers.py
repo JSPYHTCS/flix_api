4b401fe382cbdc45dd6355184527f436aaaa5817
Mateus Santos <receiveai@gmail.com>
2024-04-25 22:48:10 -0300

    .

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -3,8 +3,13 @@ from reviews.models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    title = serializers.ReadOnlyField(source='movie.title')
+    # title = serializers.ReadOnlyField(source='movie.title')
+    title = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Review
         fields = '__all__'
+    
+    def get_title(self, obj):
+        title = obj.movie.title
+        return title
